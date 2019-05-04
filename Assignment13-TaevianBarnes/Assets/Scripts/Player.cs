2ba7a5d8cf966941ae633e31f7b46207944199de
tbarnes1930 <46728262+tbarnes1930@users.noreply.github.com>
2019-05-04 13:59:27 -0400

    Updated Player and Game

---
+++ Assignment13-TaevianBarnes/Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    [SyncVar]
+    public Color color;
+
+    float moveSpeed = 3.0f;
+    float yRotate = 50f;
+
+    private void Start()
+    {
+        transform.Translate(0, 1, 0);
+    }
+
+    public override void OnStartClient()
+    {
+        gameObject.GetComponent<Renderer>().material.color = color;
+    }
+
+    void Update()
+    {
+        if (isLocalPlayer && hasAuthority)
+        {
+            Movement();
+        }
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+           
+            transform.Translate((Time.deltaTime * (moveSpeed * -1)), 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            //transform.Rotate(0, (Time.deltaTime * yRotate), 0);
+            transform.Translate((Time.deltaTime * moveSpeed), 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, (Time.deltaTime * (moveSpeed * -1)));
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, ((Time.deltaTime * yRotate) * -1), 0);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, (Time.deltaTime * yRotate), 0);
+        }
+    }
+}
